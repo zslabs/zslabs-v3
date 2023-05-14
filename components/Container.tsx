1c49472f4a6cfdd276e8d2a2aee52e1e513971f3
Zach Schnackel <info@zslabs.com>
2023-05-14 16:21:32 -0400

    component cleanup

--- components/Container.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-
-const Container: React.FC<React.HTMLAttributes<HTMLDivElement>> = (props) => {
-  return <div {...props} className="mx-auto max-w-3xl px-4" />
-}
-
-export default Container
--- components/Prose.tsx
+++ components/Prose.tsx
@@ -1,18 +1,12 @@
-'use client'
-
-import * as React from 'react'
-
-import { motion } from 'framer-motion'
-
 interface ProseProps {
   children: React.ReactNode
 }
 
 const Prose = ({ children, ...rest }: ProseProps) => {
   return (
-    <motion.div {...rest} className="prose text-slate-11">
+    <div {...rest} className="prose text-slate-11">
       {children}
-    </motion.div>
+    </div>
   )
 }
 
