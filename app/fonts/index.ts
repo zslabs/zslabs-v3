867e8153687bd928c68de376867b46fa6184d7d8
Zach Schnackel <info@zslabs.com>
2023-05-13 21:02:12 -0400

    Update index.ts

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -3,9 +3,13 @@ import localFont from 'next/font/local'
 export const manrope = localFont({
   src: './Manrope[wght].woff2',
   variable: '--font-sans',
+  weight: '400 600',
+  style: 'normal',
 })
 
 export const jetBrainsMono = localFont({
   src: './JetBrainsMono[wght].woff2',
   variable: '--font-mono',
+  weight: '400 600',
+  style: 'normal',
 })
