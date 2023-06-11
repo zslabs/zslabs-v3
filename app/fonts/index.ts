dd62969c5599fe9c6d10eb374ae438c24e2e58ad
Zach Schnackel <info@zslabs.com>
2023-06-11 10:23:20 -0400

    font

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local'
 
-export const plusJakartaSans = localFont({
-  src: './PlusJakartaSans[wght].woff2',
+export const manrope = localFont({
+  src: './Manrope[wght].woff2',
   variable: '--font-sans',
   weight: '400 600',
   style: 'normal',
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Analytics } from '@vercel/analytics/react'
 
-import { plusJakartaSans, jetBrainsMono } from './fonts'
+import { manrope, jetBrainsMono } from './fonts'
 import Providers from './providers'
 
 import BaseLayout from '~layouts/BaseLayout'
@@ -22,10 +22,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html
-      lang="en"
-      className={`${plusJakartaSans.variable} ${jetBrainsMono.variable}`}
-    >
+    <html lang="en" className={`${manrope.variable} ${jetBrainsMono.variable}`}>
       <body className="overflow-x-hidden overflow-y-scroll bg-slate-1 font-medium text-slate-12 antialiased selection:bg-primary-5 selection:text-slate-12">
         <Providers>
           <BaseLayout>{children}</BaseLayout>
