a671388cffa5617ab8cdfd147016d0eb306da718
Zach Schnackel <info@zslabs.com>
2024-06-23 11:00:24 -0400

    Update MoreArticlesLink.tsx

--- components/MoreArticlesLink.tsx
+++ components/MoreArticlesLink.tsx
@@ -3,11 +3,22 @@
 import Icon from '~components/Icon'
 import TextLink from '~components/TextLink'
 import Tooltip from '~components/Tooltip'
+import { css } from '~css/css'
 
 export default function MoreArticlesLink() {
   return (
     <Tooltip content="More articles">
-      <TextLink href="/articles">
+      <TextLink
+        href="/articles"
+        className={css({
+          borderRadius: 'full',
+          backgroundColor: 'black.a.8',
+          padding: '1',
+          fontSize: 'xl',
+          display: 'block',
+          width: 'fit',
+        })}
+      >
         <Icon name="more" />
       </TextLink>
     </Tooltip>
