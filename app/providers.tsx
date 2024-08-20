de26875f8bce5055f414ba56102570b5e2a61278
Zach Schnackel <info@zslabs.com>
2024-08-19 22:26:57 -0400

    use react-aria router provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,25 @@
 'use client'
 
 import { MotionConfig } from 'framer-motion'
+import { useRouter } from 'next/navigation'
+import { RouterProvider } from 'react-aria-components'
+
+declare module 'react-aria-components' {
+  interface RouterConfig {
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>['push']>[1]
+    >
+  }
+}
 
 import { spring } from '~helpers/styles'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <MotionConfig transition={spring}>{children}</MotionConfig>
+  let router = useRouter()
+
+  return (
+    <MotionConfig transition={spring}>
+      <RouterProvider navigate={router.push}>{children}</RouterProvider>
+    </MotionConfig>
+  )
 }
