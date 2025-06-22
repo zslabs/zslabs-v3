77dde9fe976842e81ddf2ed0d61bfb38495b83f3
Zach Schnackel <info@zslabs.com>
2025-06-21 21:44:29 -0400

    updated button styles

--- components/Button.tsx
+++ components/Button.tsx
@@ -33,6 +33,30 @@ const styles = cva({
     paddingInline: '4',
     willChange: 'transform',
 
+    _after: {
+      '--stroke': '94.892% .00288 264.626',
+      '--border': 1,
+      '--bg-size': 'calc(100% + (2px * var(--border)))',
+      '--alpha': 0,
+      '--gradient':
+        'linear-gradient(to bottom right, oklch(var(--stroke) / 0.125),oklch(var(--stroke) / 0.0625))',
+      content: '""',
+      pointerEvents: 'none',
+      position: 'absolute',
+      inset: '0',
+      background:
+        // eslint-disable-next-line
+        'var(--gradient) center center / var(--bg-size) var(--bg-size)',
+      borderRadius: 'xl',
+      borderWidth: '1px',
+      borderStyle: 'solid',
+      borderColor: 'transparent',
+      maskImage:
+        'linear-gradient(hsl(0 0% 100% / var(--alpha)), hsl(0 0% 100% / var(--alpha))), linear-gradient(hsl(0 0% 100%), hsl(0 0% 100%))',
+      maskClip: 'padding-box, border-box',
+      maskComposite: 'intersect',
+    },
+
     _hover: {
       transform: 'scale(1.05)',
       outlineColor: 'white.a.4',
@@ -41,15 +65,15 @@ const styles = cva({
   variants: {
     variation: {
       default: {
-        backgroundColor: 'black.a.8',
+        backgroundGradient: 'to-b',
+        gradientFrom: 'slate.2',
+        gradientTo: 'slate.1',
         boxShadow: 'default',
-
-        _hover: {
-          backgroundColor: 'black.a.10',
-        },
       },
       contrast: {
-        backgroundColor: 'slate.12',
+        backgroundGradient: 'to-b',
+        gradientFrom: 'slate.12',
+        gradientTo: 'slate.11',
         color: 'slate.1',
         boxShadow: 'contrast',
       },
