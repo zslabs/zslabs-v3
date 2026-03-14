d58bafd492af867d079719c98878d647fca70866
Zach Schnackel <info@zslabs.com>
2026-03-14 10:17:13 -0400

    vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import mdx from '@mdx-js/rollup'
+import babel from '@rolldown/plugin-babel'
 import rehypeShiki from '@shikijs/rehype'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import type { Element } from 'hast'
 import { nitro } from 'nitro/vite'
 import remarkFrontmatter from 'remark-frontmatter'
@@ -9,7 +10,6 @@ import remarkGfm from 'remark-gfm'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 function getCodeBlockTitle(metaRaw?: string) {
   if (!metaRaw) {
@@ -24,17 +24,8 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        // Ignore specific warning code
-        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
-          return
-        }
-        // For all other warnings, call the default handler
-        warn(warning)
-      },
-    },
+  resolve: {
+    tsconfigPaths: true,
   },
   plugins: [
     mdx({
@@ -78,25 +69,16 @@ export default defineConfig({
         ],
       ],
     }),
-    tsconfigPaths(),
     svgr({
       svgrOptions: {
         icon: true,
       },
     }),
     tanstackStart(),
-    nitro({
-      rollupConfig: {
-        onwarn(warning, warn) {
-          if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return
-          warn(warning)
-        },
-      },
-    }),
-    viteReact({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
+    nitro(),
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
     }),
   ],
 })
